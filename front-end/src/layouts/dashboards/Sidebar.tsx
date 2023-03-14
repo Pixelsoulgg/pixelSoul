ef65609fee523499dcafb393dfb745f9c6229a19
vien le <levienit@gmail.com>
2023-03-14 15:21:45 +0700

    - add layouts for mobile and desktop

---
+++ front-end/src/layouts/dashboards/Sidebar.tsx
@@ -0,0 +1,23 @@
+import { Flex, FlexProps, HStack, Image, Spacer, Text } from "@chakra-ui/react";
+import React from "react";
+import { layouts } from "../../configs/constants";
+import Menu from "./Menu";
+
+interface IProps extends FlexProps {}
+
+export default function Sidebar({ ...props }: IProps) {
+  return (
+    <Flex flexDirection="column" 
+    minH="100vh"
+    borderRight="1px solid #EAECF0"
+    minW="280px"
+    pl="20px"
+    {...props}>
+      <HStack w="full" py="32px" ml="20px">
+        <Image src="./logo.svg" sizes="content" alt="pixelSoul" />
+        <Text variant="with-title">{layouts.title}</Text>
+      </HStack>
+      <Menu mt="25px" />
+    </Flex>
+  );
+}
---
+++ front-end/src/pages/games/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function AllGames() {
+  return (
+    <div>all</div>
+  )
+}
