5118f8d626df1231c3223061cabf40ca75d717ee
Kelvin_thai <quan.thai1987@gmail.com>
2023-03-29 09:04:00 +0700

    Fix error WalletBalance Components

--- front-end/src/components/dashboards/WalletBalance.tsx
+++ front-end/src/components/dashboards/WalletBalance.tsx
@@ -13,7 +13,8 @@ export default function GoldButton() {
                 padding={{ base: "8px 12px", lg: "8px 0px" }}
               >
                 <Image src='./coin.svg' boxSize='20px' />
-                  <Text line-height= "24px" fontFamily={fonts.Inter} variant="with-title" fontSize="16px" ml="12px" weight="400" color="#667085">
+                  <Text line-height= "24px" fontFamily={fonts.Inter} variant="with-title" fontSize="16px" ml="12px" 
+                  fontWeight="400" color="#667085">
                     500 Gold
                   </Text>
                
