6366a9f057742afb73626076a526333ef3912a24
doclt <thongdoc0508@gmail.com>
2023-03-29 08:22:24 +0700

    setup prisma

---
+++ api/src/index.ts
@@ -0,0 +1,24 @@
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
+
+async function main() {
+  await prisma.users.create({
+    data: {
+      email: 'alice@prisma.io'
+    }
+  })
+
+  const allUsers = await prisma.users.findMany()
+  console.dir(allUsers, { depth: null })
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect()
+  })
+  .catch(async (e) => {
+    console.error(e)
+    await prisma.$disconnect()
+    process.exit(1)
+  })
