b04712381e294e78a1759446d4c45fdb683cc6c7
doclt <thongdoc0508@gmail.com>
2023-03-31 09:25:09 +0700

    refactor moralis service, opensea service, coin marketcap service

--- api/src/coin-market-cap/coin-market-cap.service.ts
+++ api/src/coin-market-cap/coin-market-cap.service.ts
@@ -1,23 +1,27 @@
+import { Injectable } from '@nestjs/common'
 import axios from 'axios'
 import { COINMARKETCAP_API_KEY } from '../app.settings'
 import { Currency } from './coin-market-cap.types'
 
-export async function getExchangeRate(slug: string): Promise<Currency | undefined> {
-  try {
-    const response = await axios(
-      `https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest?slug=${slug}`,
-      {
-        headers: {
-          'Content-type': 'application/json',
-          'X-CMC_PRO_API_KEY': COINMARKETCAP_API_KEY
+@Injectable()
+export class CoinMarketCapService {
+  async getExchangeRate(slug: string): Promise<Currency | undefined> {
+    try {
+      const response = await axios(
+        `https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest?slug=${slug}`,
+        {
+          headers: {
+            'Content-type': 'application/json',
+            'X-CMC_PRO_API_KEY': COINMARKETCAP_API_KEY
+          }
         }
-      }
-    )
-    const coinid = Object.keys(response.data.data)[0]
-    const rs = <Currency>response.data.data[coinid]
-    return rs
-  } catch (error) {
-    console.log(error)
+      )
+      const coinid = Object.keys(response.data.data)[0]
+      const rs = <Currency>response.data.data[coinid]
+      return rs
+    } catch (error) {
+      console.log(error)
+    }
+    return undefined
   }
-  return undefined
 }
--- api/src/score/score.module.ts
+++ api/src/score/score.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common'
+import { CoinMarketCapService } from 'src/coin-market-cap/coin-market-cap.service'
 import { PrismaService } from 'src/prisma.service'
 import { ScoreController } from './score.controller'
 import { ScoreService } from './score.service'
 
 @Module({
   controllers: [ScoreController],
-  providers: [ScoreService, PrismaService]
+  providers: [ScoreService, PrismaService, CoinMarketCapService]
 })
 export class ScoreModule {}
