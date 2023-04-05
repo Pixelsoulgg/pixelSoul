867bcb0d83cbe2d27948e41fd90652774f76f322
vien le <levienit@gmail.com>
2023-04-05 09:54:07 +0700

    Delete old source code

--- fe/src/components/animations/index.ts
+++
@@ -1,4 +0,0 @@
-export const Animate = {
-  whileHover: {scale: 1.01, boxShadow: "4px 4px 4px rgba(151, 71, 255, 0.35)" },
-  whileTap: {scale: 1.01, boxShadow: "4px 4px 4px rgba(151, 71, 255, 0.35)" }
-}
