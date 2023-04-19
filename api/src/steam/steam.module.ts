0b8c9f0438b653edc0ec976fda011b5e23cc8776
doclt <thongdoc0508@gmail.com>
2023-04-19 16:01:42 +0700

    calculated gaming activity point

--- api/src/steam/steam.module.ts
+++ api/src/steam/steam.module.ts
@@ -6,10 +6,25 @@ import { PassportModule } from '@nestjs/passport'
 import { GameService } from 'src/game/game.service'
 import { PrismaService } from 'src/prisma.service'
 import { UserService } from 'src/user/user.service'
+import { ScoreModule } from 'src/score/score.module'
+import { ScoreService } from 'src/score/score.service'
+import { CoinMarketCapService } from 'src/coin-market-cap/coin-market-cap.service'
+import { MoralisService } from 'src/moralis/moralis.service'
+import { OpenseaService } from 'src/opensea/opensea.service'
 
 @Module({
   controllers: [SteamController],
-  providers: [SteamService, SteamStrategy, GameService, PrismaService, UserService],
+  providers: [
+    SteamService,
+    SteamStrategy,
+    GameService,
+    PrismaService,
+    UserService,
+    ScoreService,
+    CoinMarketCapService,
+    MoralisService,
+    OpenseaService
+  ],
   imports: [PassportModule]
 })
 export class SteamModule {}
