facede1c13f4cc3d677a2796a5dff7dde5e646de
doclt <thongdoc0508@gmail.com>
2023-05-16 11:45:44 +0700

    added open mystery chest endpoint

--- api/src/mystery-chest/mystery-chest.controller.ts
+++ api/src/mystery-chest/mystery-chest.controller.ts
@@ -1,6 +1,7 @@
-import { Controller, Get, Param, Post } from '@nestjs/common'
+import { Body, Controller, Get, Param, Post } from '@nestjs/common'
 import { ApiTags } from '@nestjs/swagger'
 import { MysteryChestService } from './mystery-chest.service'
+import { OpenChestDto } from './dto/mystery-chest.open.dto'
 @ApiTags('mystery chest')
 @Controller({ version: '1', path: 'mystery-chest' })
 export class MysteryChestController {
@@ -12,5 +13,9 @@ export class MysteryChestController {
   @Post('claim/:auth0Sub')
   async claim(@Param('auth0Sub') auth0Sub: string) {
     return await this.mysteryChestService.increase(auth0Sub)
+  }
+  @Post('open')
+  async open(@Body() data: OpenChestDto) {
+    return await this.mysteryChestService.openMysteryChest(data)
   }
 }
