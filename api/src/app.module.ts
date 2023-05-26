8195c3af817fe0dc66241bd5297299e986d9c5e6
doclt <thongdoc0508@gmail.com>
2023-05-26 15:27:55 +0700

    fix test error

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -14,9 +14,6 @@ import { EventModule } from './event/event.module'
 import { ServeStaticModule } from '@nestjs/serve-static'
 import { join } from 'path'
 import { AuthModule } from './auth/auth.module'
-import { APP_GUARD } from '@nestjs/core'
-import { AuthGuard } from '@nestjs/passport'
-import { RolesGuard } from './roles/roles.guard'
 
 @Module({
   imports: [
