446a845d81a77c83513fff877e4c209b8c7c00ed
doclt <thongdoc0508@gmail.com>
2023-07-21 09:08:49 +0700

    changed soultag package object

--- fe/src/utils/suis/index.ts
+++ fe/src/utils/suis/index.ts
@@ -1,8 +1,13 @@
-export const CHECK_NAME_OBJECT_ID='0x7a2982a80f5dd142267cf36c940e3c2cf337806462ccc7c3efdb37e120632827';
+export const CHECK_NAME_OBJECT_ID =
+  "0x7a2982a80f5dd142267cf36c940e3c2cf337806462ccc7c3efdb37e120632827";
 
-export const coinType =`0x2505fc7fa0a7ea8b19e439e95b12b59e8762b15832c806de21f8b059f845775b::brawlz::mint_hero`;
-export const package_type ='0x2505fc7fa0a7ea8b19e439e95b12b59e8762b15832c806de21f8b059f845775b::brawlz::Hero'
-export const operator_address = '0x7589cb9fad93acf50fac1fb0add781f5418f76a37e2ebe76b0382761aa878bab';
+export const coinType = `0x2505fc7fa0a7ea8b19e439e95b12b59e8762b15832c806de21f8b059f845775b::brawlz::mint_hero`;
+export const package_type =
+  "0x2505fc7fa0a7ea8b19e439e95b12b59e8762b15832c806de21f8b059f845775b::brawlz::Hero";
+export const operator_address =
+  "0x7589cb9fad93acf50fac1fb0add781f5418f76a37e2ebe76b0382761aa878bab";
 
-export const soultag_package = '0x3dd19dc2ce054e10fb30e53645dddc5bdb5688e536ae678f12ac56545227e449::soulTag::mint';
-export const soultag_check_condition = '0x3dd19dc2ce054e10fb30e53645dddc5bdb5688e536ae678f12ac56545227e449::soulTag::SoulTag'
+export const soultag_package =
+  "0xc235858950e5d59c9ffba08dbdc237d45b53b8daf77292fb69ce2f49819038f4::soulTag::mint";
+export const soultag_check_condition =
+  "0xc235858950e5d59c9ffba08dbdc237d45b53b8daf77292fb69ce2f49819038f4::soulTag::SoulTag";
